b77b11dae71c297341980f4124f00540d5163fb2
majczuk <77607765+majczuk@users.noreply.github.com>
2021-03-01 09:08:14 +0100

    Stworzenie tablicy dwuwymiarowej

---
+++ Ćwiczenia/tablicedwuwymiarowe.java
@@ -0,0 +1,14 @@
+class tablicedwuwymiarowe{
+    public static void main(String [] args){
+        int [] tab = new int[5];
+        tab[0] = 40;
+        //tab[1] = "Jeden"; //error
+        //wyswietlanie elementow tablicy:
+        for(int i=0;i<tab.length;i++){
+            System.out.println(tab[i]);
+        }
+        //commit : Stworzenie tablicy dwuwymiarowej
+        int [][] tab2d = new int[5][2];
+
+    }
+}
